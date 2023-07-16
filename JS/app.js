06d2f516696e1321454850c146b8751de05b439e
Yein E <yeinneg@gmail.com>
2023-07-16 11:08:37 -0300

    clase activo editada y css unificado

--- JS/app.js
+++ JS/app.js
@@ -1,11 +1,22 @@
 // Asignar la clase "activo" a la etiqueta nav en HTML
 const button = document.querySelector('.btn_hamburger');
-const nav    = document.querySelector('.head_nav');
+const nav = document.querySelector('.head_nav');
+const navItems = document.querySelectorAll('.head_nav_ul_li');
 
-button.addEventListener('click',()=>{
-    nav.classList.toggle('activo')
+button.addEventListener('click', () => {
+  nav.classList.toggle('activo');
 });
 
+navItems.forEach(item => {
+  item.addEventListener('click', () => {
+    // Removemos la clase "activo" de todos los elementos
+    navItems.forEach(navItem => {
+      navItem.classList.remove('activo');
+    });
+    // Agregamos la clase "activo" solo al elemento que se hizo clic
+    item.classList.add('activo');
+  });
+});
 
 // Cerrar el menu hamburguesa (nav) al hacer click en uno de sus elementos
 
